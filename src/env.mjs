ece773a03f3e75ff87fe99c55415af61748b7c95
GoyoStach <guillaume.excoffier86@gmail.com>
2023-03-19 13:32:19 +0100

    Adding NODE_ENV

--- src/env.mjs
+++ src/env.mjs
@@ -5,7 +5,7 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 const server = z.object({
- 
+  NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
 /**
